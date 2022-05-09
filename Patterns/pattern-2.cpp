f97ff8b117630fe0dd60a690ca6e899dfd281e6e
Kishore2496 <kishore2496@gmail.com>
2022-05-09 20:24:59 +0530

    patterns1-5

---
+++ Patterns/pattern-2.cpp
@@ -0,0 +1,17 @@
+//https://nados.io/question/pattern-2?zen=true
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+    
+    //write your code here
+    for(int i = n; i > 0 ;i--) {
+       for(int j = 1; j <= i; j++) {
+           cout << "*\t";
+       }
+       cout << endl;
+   }
+}
---
+++ Patterns/pattern-3.cpp
@@ -0,0 +1,18 @@
+//https://nados.io/question/pattern-3?zen=true
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+    
+    //write your code here
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++){
+            if(j > n-i) cout << "*\t";
+            else         cout << "\t";
+        }
+        cout << endl;
+    }
+}
